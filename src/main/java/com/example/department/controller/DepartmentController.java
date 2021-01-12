eeafdeaca9fddb160acea1b40e8db50bf6b1a524
bandanarama <kvoon0@frostburg.edu>
2021-01-11 22:55:22 -0700

    controller, department, repository, service classes created. Working on Rest.

---
+++ src/main/java/com/example/department/controller/DepartmentController.java
@@ -0,0 +1,31 @@
+package com.example.department.controller;
+
+import com.example.department.entity.Department;
+import com.example.department.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/departments")
+@Slf4j
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/")
+    public Department saveDepartment(@RequestBody Department department) {
+        Log.info("Inside saveDepartment method of Department Controller")
+        return departmentService.saveDepartment(department);
+
+    }
+
+    public Department getDepartment(Department department) {
+
+
+    }
+}
--- src/main/java/com/example/department/entity/Department.java
+++ src/main/java/com/example/department/entity/Department.java
@@ -1,9 +1,25 @@
 package com.example.department.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
-public class Department {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
-    private Long de
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+
 }
